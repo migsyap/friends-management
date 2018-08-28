5a76fdfc08112a943e345f4ab3e839ec391d29aa
Miguel Yap <migueleduardo.yap@gmail.com>
2018-08-28 22:28:36 +0800

    Add tests for the controller

--- src/test/java/com/sp/component/RestApisTest.java
+++ src/test/java/com/sp/component/RestApisTest.java
@@ -1,7 +1,9 @@
 package com.sp.component;
 
 import com.sp.dto.ConnectionRequest;
+import com.sp.dto.FindConnectionsRequest;
 import com.sp.dto.Response;
+import com.sp.dto.SubscriptionRequest;
 import com.sp.exception.BusinessException;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
@@ -12,11 +14,13 @@ import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
 
 public class RestApisTest {
     @InjectMocks
@@ -59,6 +63,136 @@ public class RestApisTest {
 
         r = (Response) resp.getBody();
         assert (Objects.nonNull(r));
+        assert (r.isSuccess());
+        assert (StringUtils.isBlank(r.getMessage()));
+    }
+
+    @Test
+    public void findConnections() {
+        FindConnectionsRequest request = FindConnectionsRequest.builder().email("mike@mail.com").build();
+        Response r;
+
+        when(manager.findConnections(eq(request))).thenReturn(Collections.emptyList());
+        ResponseEntity resp = controller.findConnections(request);
+        assert (HttpStatus.OK.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (r.isSuccess());
+        assert (r.getFriends().isEmpty());
+        assert (r.getCount() == 0);
+
+        when(manager.findConnections(eq(request))).thenReturn(Collections.singletonList("george@mail.com"));
+        resp = controller.findConnections(request);
+        assert (HttpStatus.OK.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (r.isSuccess());
+        assert (!r.getFriends().isEmpty());
+        assert (r.getCount() == 1);
+    }
+
+    @Test
+    public void findCommonConnections__business_exception() throws BusinessException {
+        ResponseEntity resp;
+        Response r;
+
+        Mockito.doThrow(new BusinessException("test exception")).when(manager).findCommonConnections(any(ConnectionRequest.class));
+        resp = controller.findCommonConnections(ConnectionRequest.builder().build());
+        assert (HttpStatus.NOT_ACCEPTABLE.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (Objects.nonNull(r));
+        assert (!r.isSuccess());
+        assert ("test exception".equals(r.getMessage()));
+    }
+
+    @Test
+    public void findCommonConnections__success() throws BusinessException {
+        ConnectionRequest request = ConnectionRequest.builder().friends(Arrays.asList("mike@mail.com", "george@mail.com")).build();
+        Response r;
+
+        when(manager.findCommonConnections(eq(request))).thenReturn(Collections.emptyList());
+        ResponseEntity resp = controller.findCommonConnections(request);
+        assert (HttpStatus.OK.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (r.isSuccess());
+        assert (r.getFriends().isEmpty());
+        assert (r.getCount() == 0);
+
+        when(manager.findCommonConnections(eq(request))).thenReturn(Collections.singletonList("matt@mail.com"));
+        resp = controller.findCommonConnections(request);
+        assert (HttpStatus.OK.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (r.isSuccess());
+        assert (!r.getFriends().isEmpty());
+        assert (r.getCount() == 1);
+    }
+
+    @Test
+    public void subscribe__business_exception() throws BusinessException {
+        ResponseEntity resp;
+        Response r;
+
+        Mockito.doThrow(new BusinessException("test exception")).when(manager).subscribe(any(SubscriptionRequest.class));
+        resp = controller.subscribe(SubscriptionRequest.builder().build());
+        assert (HttpStatus.NOT_ACCEPTABLE.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (Objects.nonNull(r));
+        assert (!r.isSuccess());
+        assert ("test exception".equals(r.getMessage()));
+    }
+
+    @Test
+    public void subscribe__success() throws BusinessException {
+        SubscriptionRequest request = SubscriptionRequest.builder().requestor("mike@mail.com").target("george@mail.com").build();
+        Response r;
+
+        doNothing().when(manager).subscribe(eq(request));
+        ResponseEntity resp = controller.subscribe(request);
+        assert (HttpStatus.OK.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (r.isSuccess());
+        assert (StringUtils.isBlank(r.getMessage()));
+    }
+
+    @Test
+    public void block__business_exception() throws BusinessException {
+        ResponseEntity resp;
+        Response r;
+
+        Mockito.doThrow(new BusinessException("test exception")).when(manager).block(any(SubscriptionRequest.class));
+        resp = controller.block(SubscriptionRequest.builder().build());
+        assert (HttpStatus.NOT_ACCEPTABLE.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
+        assert (Objects.nonNull(r));
+        assert (!r.isSuccess());
+        assert ("test exception".equals(r.getMessage()));
+    }
+
+    @Test
+    public void block__success() throws BusinessException {
+        SubscriptionRequest request = SubscriptionRequest.builder().requestor("mike@mail.com").target("george@mail.com").build();
+        Response r;
+
+        doNothing().when(manager).block(eq(request));
+        ResponseEntity resp = controller.block(request);
+        assert (HttpStatus.OK.equals(resp.getStatusCode()));
+        assert (resp.getBody() instanceof Response);
+
+        r = (Response) resp.getBody();
         assert (r.isSuccess());
         assert (StringUtils.isBlank(r.getMessage()));
     }
